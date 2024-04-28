bd047b9fed2fad6b075f84146b059bfb947c66e4
erayturkay <107153012+erayturkay@users.noreply.github.com>
2024-04-28 04:07:17 +0300

    Update and rename vaft-ublock-origin neww.js to vaft-ublock-origin neew.js

--- vaft-ublock-origin neew.js
+++ vaft-ublock-origin neew.js
@@ -7,9 +7,7 @@ twitch-videoad.js text/javascript
         scope.OPT_MODE_STRIP_AD_SEGMENTS = true;
         scope.OPT_MODE_NOTIFY_ADS_WATCHED = true;
         scope.OPT_MODE_NOTIFY_ADS_WATCHED_MIN_REQUESTS = false;
-        scope.OPT_BACKUP_PLAYER_TYPE = 'embed'; //Source
         scope.OPT_REGULAR_PLAYER_TYPE = 'site'; //Source
-        scope.OPT_BACKUP_PLAYER_TYPE = 'autoplay'; //360p
         scope.OPT_BACKUP_PLATFORM = 'ios';
         scope.OPT_ACCESS_TOKEN_PLAYER_TYPE = null;
         scope.OPT_SHOW_AD_BANNER = true;
